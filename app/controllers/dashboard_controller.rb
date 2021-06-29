31af88084321d0b255cd7aa5946319a1a89db7ef
Yiorgos Michokostas <michokostas@gmail.com>
2021-06-29 17:10:26 +0200

    Update the dashboard table
    Show for each day the recorded meals.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @meals = current_user.meals
+    @meals = current_user.meals.order(:meal_type).group_by { _1.created_at.to_date }
     respond_to :html
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'active_support/testing/time_helpers'
+include ActiveSupport::Testing::TimeHelpers
+
+user = User.create(email: "a@example.com", password: "password")
+
+travel_to 2.day.ago do
+  user.meals.create(meal_type: "dinner", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
+  user.meals.create(meal_type: "lunch", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
+end
+
+travel_to 1.day.ago do
+  user.meals.create(meal_type: "dinner", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
+  user.meals.create(meal_type: "lunch", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
+end
+
+user.meals.create(meal_type: "dinner", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
+user.meals.create(meal_type: "lunch", calories: 1.1, fats: 10.1, proteins: 9.1, carbohydrates: 450.1)
